b37c8d9b021e6adacbc8346fcab69e8b98ec347a
ushou2005 <usho_feat_fubuki@yahoo.co.jp>
2023-11-04 18:55:43 +0900

    Create ScrollScript.cs

---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    [SerializeField] float ScrollSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-ScrollSpeed * Time.deltaTime, 0, 0);
+        if (transform.position.x < -5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
