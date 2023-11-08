4fcdb7d3f6f0f34cb69e60f6aa232b1b5442aeca
ushou2005 <usho_feat_fubuki@yahoo.co.jp>
2023-11-09 01:05:05 +0900

    Update StageGenerate.cs

--- Assets/Scripts/StageGenerate.cs
+++ Assets/Scripts/StageGenerate.cs
@@ -10,7 +10,7 @@ public class StageGenerate : MonoBehaviour
 
     [SerializeField] Vector3 GeneratePos;
 
-    int[] array = {0,1,0,1,0,1,0,1,0,0,0,0};
+    [SerializeField]int[] array = {0,1,0,1,0,1,0,1,0,0,0,0};
     // Start is called before the first frame update
     void Start()
     {
