c552d9ec4ef6425983b3cc67a8a5d91099391059
ushou2005 <usho_feat_fubuki@yahoo.co.jp>
2023-11-11 22:04:50 +0900

    Update EnemyScript.cs

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -10,14 +10,5 @@ using UnityEditor;
 public class EnemyScript : EnemyBaseStatus
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
