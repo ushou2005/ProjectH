69a619dbc189daa6d961bd03acf64908d37ce27f
ushou2005 <usho_feat_fubuki@yahoo.co.jp>
2023-11-11 21:41:34 +0900

    Create EnemyScript.cs

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(EnemyBaseStatus))]
+#endif
+
+public class EnemyScript : EnemyBaseStatus
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
